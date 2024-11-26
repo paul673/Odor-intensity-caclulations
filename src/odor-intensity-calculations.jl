a47e0e0b874bbe54276e2fa1e2f42018400a7dab
Paul Groening <paul.jonathan.groening@gmail.com>
2024-11-26 15:55:21 +0100

    path changes

--- src/odor-intensity-calculations.jl
+++ src/odor-intensity-calculations.jl
@@ -16,25 +16,25 @@ module odor
 
     export get_mixture_activity_coeff
 
-#df = DataFrame(CSV.File("smiles_pine.csv"))
+    df = DataFrame(CSV.File("src/smiles_pine.csv"))
 
-#basic_smiles = String.(df.basic_smiles)
-#smiles = String.(df.smiles)
-#new_smiles = []
-#new_comp = []
-#for i in eachindex(smiles)
-#    println(smiles[i])
- #   if smiles[i] in ["C[C@@]12CC[C@@H]3[C@H](CC3(C)C)C(=C)CC[C@H]1O2", "CC1=C[C@H]2[C@@H](CCC(=C2CC1)C)C(C)C",r"C/C/1=C\CCC(=C)[C@H]2CC([C@@H]2CC1)(C)C"]
- ##       println("stop")
- #   else
- #       push!(new_smiles, smiles[i])
- #       push!(new_comp,df.composition[i])
- #   end
-#end
+    basic_smiles = String.(df.basic_smiles)
+    smiles = String.(df.smiles)
+    new_smiles = []
+    new_comp = []
+    for i in eachindex(smiles)
+        println(smiles[i])
+       if smiles[i] in ["C[C@@]12CC[C@@H]3[C@H](CC3(C)C)C(=C)CC[C@H]1O2", "CC1=C[C@H]2[C@@H](CCC(=C2CC1)C)C(C)C",r"C/C/1=C\CCC(=C)[C@H]2CC([C@@H]2CC1)(C)C"]
+           println("stop")
+       else
+           push!(new_smiles, smiles[i])
+           push!(new_comp,df.composition[i])
+       end
+    end
 
-#new_smiles
-#get_mixture_activity_coeff(new_smiles ,new_comp)
+    #new_smiles
+    println(get_mixture_activity_coeff(new_smiles ,new_comp))
 
-#get_mixture_activity_coeff(["CCC"] ,[1])
+    #get_mixture_activity_coeff(["CCC"] ,[1])
 
 end # module odor
